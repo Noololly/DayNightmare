0e95a0868dd650242c8eab1cbe692428db24e37d
Lenny Kay <77846195+lenbot7@users.noreply.github.com>
2025-09-28 14:59:34 +0100

    Gravity works!

--- scenes/heidi.gd
+++ scenes/heidi.gd
@@ -11,21 +11,20 @@ func _ready():
 	# Make sure gravity_scale is correctly set (affects gravity strength)
 	gravity_scale = gravity_scale
 
-
 func _physics_process(delta: float) -> void:
 	var direction := 0.0
 	
 	# Horizontal movement based on key input
-	if Input.is_action_pressed("orph_left"):
+	if Input.is_action_pressed("heidi_left"):
 		direction -= 1
-	if Input.is_action_pressed("orph_right"):
+	if Input.is_action_pressed("heidi_right"):
 		direction += 1
 	
 	# Set horizontal velocity directly for continuous movement
 	linear_velocity.x = direction * speed
 	
 	# Jumping logic: Only jump if on the floor (RayCast2D detects the ground)
-	if Input.is_action_just_pressed("orph_jump") and is_on_floor():
+	if Input.is_action_just_pressed("heidi_jump") and is_on_floor():
 		jump()
 
 func jump():
